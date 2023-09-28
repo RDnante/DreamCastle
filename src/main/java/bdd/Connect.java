7b9e29f0319f5fd8f6f20a5abb01f6bca7488e54
JerrickRakotonirina <119698031+JerrickRakotonirina@users.noreply.github.com>
2023-09-28 13:39:12 +0300

    Connect

--- src/main/java/bdd/Connect.java
+++ src/main/java/bdd/Connect.java
@@ -1,5 +1,14 @@
 package bdd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Connect {
-    
+    public static Connection seConnecterPostgre(String user , String mdp , String base) throws Exception
+    {
+        Class.forName("org.postgresql.Driver");
+        String url="jdbc:postgresql://localhost:5432/"+base;
+        Connection con= DriverManager.getConnection(url,user,mdp);
+        return con;
+    }
 }
